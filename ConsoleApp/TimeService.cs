88cf0bb467c5ae885b79d3a51645935d3b197311
Maribel Granada <156698686+MaribelGranada@users.noreply.github.com>
2025-09-06 14:58:51 -0500

    Update TimeService.cs

--- ConsoleApp/TimeService.cs
+++ ConsoleApp/TimeService.cs
@@ -6,13 +6,13 @@ namespace ConsoleApp.Services
 {
     public static class TimeService
     {
-        // Imprime la información exactamente en el formato esperado
+       
         public static void PrintTimeInfo(Time t, Time sumWith, Time compareWith)
         {
-            // Línea de tiempo
+
             Console.WriteLine($"Time: {t}");
 
-            // Números con separador de miles y alineados a la derecha (igual que el ejemplo)
+     
             string ms = t.ToMilliseconds().ToString("N0", CultureInfo.InvariantCulture);
             string sec = t.ToSeconds().ToString("N0", CultureInfo.InvariantCulture);
             string min = t.ToMinutes().ToString("N0", CultureInfo.InvariantCulture);
@@ -21,7 +21,7 @@ namespace ConsoleApp.Services
             Console.WriteLine($"    Seconds     : {sec,15}");
             Console.WriteLine($"    Minutes     : {min,15}");
 
-            // Add y Is Other day
+
             Console.WriteLine($"    Add         : {t.Add(sumWith)}");
             Console.WriteLine($"    Is Other day: {t.IsOtherDay(compareWith)}");
 
