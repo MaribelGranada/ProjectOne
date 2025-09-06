dbb7a38d206094855941b71d55ee9deb316fcc76
MaribelGranada <tuemail@ejemplo.com>
2025-09-06 14:56:11 -0500

    estreucturaDatos1

--- ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd858c83874f33086d24d70939c48956d4dc037")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e887dda7c127c998412f0ed0f0c4143bea415d03")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
