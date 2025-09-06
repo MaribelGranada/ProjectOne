0c30ca81a65a0e2c0a87f42c814c9fa0ab83d029
Maribel Granada <156698686+MaribelGranada@users.noreply.github.com>
2025-09-06 14:58:04 -0500

    Update Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp
         {
             try
             {
-                // Definición exacta de las 5 horas (en 24h input)
+                
                 Time t1 = new Time(0, 0, 0, 0);           // 00:00:00.000
                 Time t2 = new Time(14, 0, 0, 0);          // 14:00:00.000 (=> 02:00:00.000 PM)
                 Time t3 = new Time(9, 34, 0, 0);          // 09:34:00.000 (se usa para Add)
@@ -19,13 +19,13 @@ namespace ConsoleApp
 
                 Time[] times = { t1, t2, t3, t4, t5 };
 
-                // Para cada tiempo: imprimir info, Add con t3 y IsOtherDay con t4
+                
                 foreach (var t in times)
                 {
                     TimeService.PrintTimeInfo(t, t3, t4);
                 }
 
-                // Probar excepción (la salida debe ser exactamente: "The hour: 45, is not valid.")
+                
                 try
                 {
                     Time bad = new Time(45, 0, 0, 0);
